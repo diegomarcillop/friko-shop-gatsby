c9d1f3abdc634db4bbf9f13c94380491d6f421cb
diegomarcillop <dfmarcillop@gmail.com>
2020-12-05 11:04:04 -0500

    feat (CardProduct): added product card

---
+++ src/components/CardProduct/CardProduct.js
@@ -0,0 +1,42 @@
+import { Button, Card, Col, Row } from "antd"
+import React from "react"
+import Image from "../Image/Image"
+import { HeartOutlined, PlusOutlined } from "@ant-design/icons"
+
+export default function CardProduct({ data }) {
+  return (
+    <Card className="cardProduct">
+      <Image name="product_1" className="cardProduct__image" />
+      <h2 className="cardProduct__name">{data.name}</h2>
+
+      <Row justify="space-between" className="cardProduct__content">
+        <Col lg={12} xs={12}>
+          <h4 className="cardProduct__price">
+            <strong>$</strong>
+            {data.price}
+          </h4>
+        </Col>
+        <Col lg={12} xs={12}>
+          <Row justify="end">
+            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 6, offset: 2 }}>
+              <Button
+                size="large"
+                block
+                icon={<HeartOutlined />}
+                className="cardProduct__buttonHeart"
+              />
+            </Col>
+            <Col lg={{ span: 6, offset: 3 }} xs={{ span: 6, offset: 2 }}>
+              <Button
+                className="cardProduct__buttonPlus"
+                block
+                size="large"
+                icon={<PlusOutlined />}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
+  )
+}
