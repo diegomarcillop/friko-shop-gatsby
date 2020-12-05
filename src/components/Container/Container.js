6ffc3a6b496101a1858a271badb6e419984dc450
diegomarcillop <dfmarcillop@gmail.com>
2020-12-05 11:01:32 -0500

    fix (Container): container dimensions

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,21 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Layout } from "antd"
-import Header from "./Header/Header"
+import Header from "../Header/Header"
 const { Content } = Layout
 
 const Container = ({ children }) => {
   return (
     <Layout>
       <Header />
-      <Content
-        style={{
-          padding: "24px 50px",
-          marginTop: 64,
-          background: `#fff`,
-          minHeight: "100vh", //edit this to change minimum page height
-        }}
-      >
+      <Content className="container">
         <div>{children}</div>
       </Content>
     </Layout>
